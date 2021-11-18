098671514d4a3a0d6617ec3dda522e0d63f553a3
cooldev900 <cooldev900@gmail.com>
2021-11-18 21:46:00 +0300

    Add nationalcode seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // Seed one time reference data
-        $this->call(Scopes::class);
-        $this->call(Roles::class);
-        $this->call(StoreViews::class);
-        $this->call(Users::class);
-        $this->call(UserPermissions::class);
         $this->call(Companies::class);
+        $this->call(NationalCodes::class);
+        $this->call(Roles::class);
+        $this->call(Scopes::class);
+        $this->call(StoreViews::class);
+        $this->call(UserPermissions::class);
+        $this->call(Users::class);
     }
 }
